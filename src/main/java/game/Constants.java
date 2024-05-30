7797477dd4ba87268e09e10a5fe0a90e62153b81
Pradeep <impradeeparya@yahoo.com>
2024-05-31 01:16:12 +0530

    bubble shooter init

---
+++ src/main/java/game/Constants.java
@@ -0,0 +1,11 @@
+package game;
+
+//this class stores only the constants that define the layout
+
+public class Constants {
+	public static final int WINDOW_SIZE_X = 640;
+	public static final int WINDOW_SIZE_Y = 480;
+	public static final int FIELD_SIZE_X = 420;
+	public static final int FIELD_SIZE_Y = 480;
+	public static final int ROW_DISTANCE = 52;
+}
---
+++ src/main/java/game/Main.java
@@ -0,0 +1,10 @@
+package game;
+
+//main class, simply initiates the game
+
+public class Main {
+	public static void main(String[] args) {
+		MainFrame gameFrame = new MainFrame();
+		gameFrame.init();
+	}
+}
